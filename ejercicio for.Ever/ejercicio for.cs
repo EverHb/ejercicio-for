b04f778d0e45bac09e5178b5f3343c3e344cf9c3
EverHb <everbenavides1234@gmail.com>
2023-03-08 21:50:04 -0600

    ejercicio for

---
+++ ejercicio for.Ever/ejercicio for.cs
@@ -0,0 +1,14 @@
+﻿using System ; 
+class Program { 
+    static void Main(string[] args) {
+        Console.Write("Introduce tu número entero: ");
+        int altura = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j <= i; j++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
