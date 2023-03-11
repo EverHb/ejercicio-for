5b85e75786573e5ba94dfd866553bcdb45bb2497
EverHb <124007232+EverHb@users.noreply.github.com>
2023-03-10 23:42:50 -0600

    Update ejercicio for.cs

--- ejercicio for.Ever/ejercicio for.cs
+++ ejercicio for.Ever/ejercicio for.cs
@@ -1,4 +1,4 @@
-﻿using System ; 
+using Syste; 
 class Program { 
     static void Main(string[] args) {
         Console.Write("Introduce tu número entero: ");
@@ -6,7 +6,7 @@ class Program {
 
         for (int i = 0; i < altura; i++) {
             for (int j = 0; j <= i; j++) {
-                Console.Write("*");
+                Console.Write("Altura");
             }
             Console.WriteLine();
         }
